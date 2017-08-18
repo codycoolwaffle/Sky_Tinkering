4b6c398a52442f2beea0b271b23b3e1641e75a79
codycoolwaffle <codyhaslam@gmail.com>
2017-08-18 00:50:10 -0700

    API fix

---
+++ src/main/java/api/exnihilo/CrookUtils.java
@@ -0,0 +1,12 @@
+package exnihilo.utils;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+public class CrookUtils
+{
+	public static boolean doCrooking(ItemStack item, int X, int Y, int Z, EntityPlayer player)
+	{
+		return true;
+	}
+}
